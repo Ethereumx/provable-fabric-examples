150274af87cb8b9d524085e9056949b70e993463
Chris Hill <chrishill399@gmail.com>
2020-08-10 10:10:39 +0100

    match fabric 2.x chaincode template

--- eurusd-example/chaincode/go/user-chaincode.go
+++ eurusd-example/chaincode/go/user-chaincode.go
@@ -3,47 +3,43 @@ package main
 import (
 	"fmt"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	sc "github.com/hyperledger/fabric/protos/peer"
-	oraclizeapi "github.com/oraclize/fabric-api"
+	oraclizeapi "https://github.com/provable-things/fabric-api"
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
 // SmartContract defines the Smart Contract structure
 type SmartContract struct {
+	contractapi.Contract
 }
 
-func (s *SmartContract) Init(APIstub shim.ChaincodeStubInterface) sc.Response {
-	return shim.Success(nil)
+func (s *SmartContract) Init(ctx contractapi.TransactionContextInterface) error {
+	return nil
 }
 
-func (s *SmartContract) Invoke(APIstub shim.ChaincodeStubInterface) sc.Response {
-	// Retrieve the requested Smart Contract function and arguments
-	function, args := APIstub.GetFunctionAndParameters()
-	// Route to the appropriate handler function to interact with the ledger appropriately
-	if function == "fetchEURUSDviaOraclize" {
-		return s.fetchEURUSDviaOraclize(APIstub)
-	}
-	fmt.Println("function:", function, args[0])
-	return shim.Error("Invalid Smart Contract function name.")
-}
 
-func (s *SmartContract) fetchEURUSDviaOraclize(APIstub shim.ChaincodeStubInterface) sc.Response {
+func (s *SmartContract) FetchEURUSDviaOraclize(ctx contractapi.TransactionContextInterface) error {
 	fmt.Println("============= START : Calling the oraclize chaincode =============")
 	var datasource = "URL"                                                                  // Setting the Oraclize datasource
 	var query = "json(https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=USD).USD" // Setting the query
-	result, proof := oraclizeapi.OraclizeQuery_sync(APIstub, datasource, query, oraclizeapi.TLSNOTARY)
+	result, proof := oraclizeapi.OraclizeQuery_sync(ctx.GetStub(), datasource, query, oraclizeapi.TLSNOTARY)
 	fmt.Printf("proof: %s", proof)
 	fmt.Printf("\nresult: %s\n", result)
 	fmt.Println("Do something with the result...")
 	fmt.Println("============= END : Calling the oraclize chaincode =============")
-	return shim.Success(result)
+	
+	return nil
 }
 
 // The main function is only relevant in unit test mode. Only included here for completeness.
 func main() {
-	// Create a new Smart Contract
-	err := shim.Start(new(SmartContract))
+	chaincode, err := contractapi.NewChaincode(new(SmartContract))
+
 	if err != nil {
-		fmt.Printf("Error creating new Smart Contract: %s", err)
+		fmt.Printf("Error creating chaincode: %s", err.Error())
+		return
+	}
+
+	if err := chaincode.Start(); err != nil {
+		fmt.Printf("Error starting chaincode: %s", err.Error())
 	}
 }
